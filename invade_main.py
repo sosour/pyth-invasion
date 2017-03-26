a3381f5294c984b2b0c75381abe38f1082b14084
Agn Sau <agn.zygula@gmail.com>
2017-03-26 17:40:49 +0200

    Creating sloth

--- invade_main.py
+++ invade_main.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 
 from settings import Settings
+from sloth import Sloth
+
 
 def run_game():
     pygame.init()
@@ -9,12 +11,16 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Invasion")
 
+    sloth = Sloth(screen)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
         
         screen.fill(ai_settings.bg_color)
+        sloth.blitme()
+
         pygame.display.flip()
 
 run_game()
--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ class Settings():
     """Class for keeping all of the game setting"""
 
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (255,226,0)
+        self.screen_width = 1080
+        self.screen_height = 720
+        self.bg_color = (26, 35, 126)
